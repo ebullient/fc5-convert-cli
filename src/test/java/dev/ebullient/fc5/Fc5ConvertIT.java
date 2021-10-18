dd8f96bcfa768f0d519f3e402f6de1a49985af7c
Erin Schnabel <erinschnabel@gmail.com>
2021-10-17 23:45:27 -0400

    Use QuarkusMainTest

---
+++ src/test/java/dev/ebullient/fc5/Fc5ConvertIT.java
@@ -0,0 +1,8 @@
+package dev.ebullient.fc5;
+
+import io.quarkus.test.junit.main.QuarkusMainIntegrationTest;
+
+@QuarkusMainIntegrationTest
+public class Fc5ConvertIT extends Fc5ConvertTest {
+
+}
---
+++ src/test/java/dev/ebullient/fc5/Fc5ConvertTest.java
@@ -0,0 +1,111 @@
+package dev.ebullient.fc5;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Comparator;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
+
+import io.quarkus.test.junit.main.Launch;
+import io.quarkus.test.junit.main.LaunchResult;
+import io.quarkus.test.junit.main.QuarkusMainLauncher;
+import io.quarkus.test.junit.main.QuarkusMainTest;
+
+@QuarkusMainTest
+public class Fc5ConvertTest {
+    final static Path PROJECT_PATH = Paths.get(System.getProperty("user.dir")).toAbsolutePath();
+    final static Path OUTPUT_PATH = PROJECT_PATH.resolve("target/fc5-convert");
+
+    @Test
+    @Launch({})
+    void testThings() {
+        System.out.println("Life is good");
+    }
+
+    @Test
+    @Launch({ "--help" })
+    void testCommandHelp(LaunchResult result) {
+        System.out.println(result.getOutput());
+        Assertions.assertTrue(result.getOutput().contains("Usage: fc5-convert [-hvV] [<input>...] [COMMAND]"),
+                "Result should contain the CLI help message. Found: " + dump(result));
+    }
+
+    @Test
+    @Launch({ "obsidian", "--help" })
+    void testObsidianCommandHelp(LaunchResult result) {
+        System.out.println(result.getOutput());
+        Assertions.assertTrue(result.getOutput().contains("Usage: fc5-convert obsidian [-hvV] -o=<outputPath> [<input>...]"),
+                "Result should contain the CLI help message. Found: " + dump(result));
+    }
+
+    @Test
+    @Launch({ "transform", "--help" })
+    void testTransformCommandHelp(LaunchResult result) {
+        System.out.println(result.getOutput());
+        Assertions.assertTrue(
+                result.getOutput().contains("Usage: fc5-convert transform [-hvV] -o=<outputPath> [-t=<xsltFile>]"),
+                "Result should contain the CLI help message. Found: " + dump(result));
+    }
+
+    @Test
+    @Launch({ "validate", "--help" })
+    void testValidateCommandHelp(LaunchResult result) {
+        System.out.println(result.getOutput());
+        Assertions.assertTrue(
+                result.getOutput().contains("Usage: fc5-convert validate [-hvV] [--collection | [--compendium] |"),
+                "Result should contain the CLI help message. Found: " + dump(result));
+    }
+
+    @Test
+    void testFc5Xml(TestInfo info, QuarkusMainLauncher launcher) {
+        LaunchResult result;
+
+        result = launcher.launch("validate", "--collection", "./src/test/resources/FC5-Collection.xml");
+        Assertions.assertEquals(0, result.exitCode(), "An error occurred. " + dump(result));
+
+        result = launcher.launch("validate", "--compendium", "src/test/resources/FC5-Compendium.xml");
+        Assertions.assertEquals(0, result.exitCode(), "An error occurred. " + dump(result));
+
+        result = launcher.launch("transform", "-o", OUTPUT_PATH.toString(), "-x", "-merged",
+                "src/test/resources/FC5-Collection.xml");
+        Assertions.assertEquals(0, result.exitCode(), "An error occurred. " + dump(result));
+
+        result = launcher.launch("validate", "--compendium", OUTPUT_PATH.resolve("FC5-Collection-merged.xml").toString());
+        Assertions.assertEquals(0, result.exitCode(), "An error occurred. " + dump(result));
+
+        result = launcher.launch("validate", "--compendium", OUTPUT_PATH.resolve("FC5-Collection-merged.xml").toString());
+        Assertions.assertEquals(0, result.exitCode(), "An error occurred. " + dump(result));
+
+        result = launcher.launch("obsidian", "-o", OUTPUT_PATH.toString(),
+                OUTPUT_PATH.resolve("FC5-Collection-merged.xml").toString());
+
+        Assertions.assertEquals(0, result.exitCode(), "An error occurred. " + dump(result));
+    }
+
+    public static void deleteDir(Path path) {
+        if (!path.toFile().exists()) {
+            return;
+        }
+
+        try {
+            Files.walk(path)
+                    .sorted(Comparator.reverseOrder())
+                    .map(Path::toFile)
+                    .forEach(File::delete);
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+
+        Assertions.assertFalse(path.toFile().exists());
+    }
+
+    String dump(LaunchResult result) {
+        return "\n" + result.getOutput() + "\nSystem err:\n" + result.getErrorOutput();
+    }
+}
